517630c8b5a0313b94fb663e643220c181ff0736
Rob Samson <rhsamson22@gmail.com>
2025-12-17 19:30:34 -0500

    Use repository query for latest daily metric

--- src/main/java/com/rob/health_tracker/DailyMetricRepository.java
+++ src/main/java/com/rob/health_tracker/DailyMetricRepository.java
@@ -3,4 +3,6 @@ package com.rob.health_tracker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DailyMetricRepository extends JpaRepository<DailyMetric, Long> {
+    DailyMetric findTopByOrderByDateDesc();
+
 }
